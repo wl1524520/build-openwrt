691194a39ae6d26d7f9c6203c928a6a7030651d7
Wang Lu <wl1524520@gmail.com>
2019-12-25 16:58:16 +0800

    add bind-dig

--- build_x86.sh
+++ build_x86.sh
@@ -9,7 +9,7 @@ PK_VPN="luci-app-openvpn-server luci-i18n-openvpn-server-zh-cn"
 PK_THEME="luci-theme-bootstrap luci-theme-netgear luci-theme-argon luci-theme-material luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
 # PK_DDNS="-luci-app-ddns -luci-i18n-ddns-zh-cn -ddns-scripts_aliyun -ddns-scripts_dnspod luci-app-dnspod"
 PK_DDNS="luci-app-ddns luci-i18n-ddns-zh-cn luci-app-dnspod"
-PK_ADD="luci-mod-rpc smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn luci-app-frpc luci-i18n-arpbind-zh-cn luci-i18n-autoreboot-zh-cn luci-i18n-flowoffload-zh-cn luci-i18n-nlbwmon-zh-cn luci-i18n-ramfree-zh-cn luci-i18n-upnp-zh-cn luci-i18n-wol-zh-cn coreutils-nohup"
+PK_ADD="luci-mod-rpc smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn luci-app-frpc luci-i18n-arpbind-zh-cn luci-i18n-autoreboot-zh-cn luci-i18n-flowoffload-zh-cn luci-i18n-nlbwmon-zh-cn luci-i18n-ramfree-zh-cn luci-i18n-upnp-zh-cn luci-i18n-wol-zh-cn coreutils-nohup bind-dig"
 PK_DEL=" -alsa-utils -automount -autosamba -block-mount -kmod-8139cp -kmod-8139too -kmod-r8125 -kmod-ac97 -kmod-ath10k -kmod-ath5k -kmod-ath9k -kmod-ath9k-htc -kmod-rt2800-usb -kmod-sound-hda-codec-realtek -kmod-sound-hda-codec-via -kmod-sound-hda-core -kmod-sound-via82xx -kmod-usb-audio -kmod-usb-net -kmod-usb-net-asix -kmod-usb-net-asix-ax88179 -kmod-usb-net-rtl8150 -kmod-usb-net-rtl8152 -luci-app-accesscontrol -luci-app-adbyby-plus -luci-app-filetransfer -luci-app-ipsec-vpnd -luci-app-pptp-server -luci-app-sfe -luci-app-transmission -luci-app-v2ray-server -luci-app-vlmcsd -luci-app-vsftpd -luci-app-xlnetacc -luci-app-zerotier -wpad -luci-app-qbittorrent"
 PK_DEL_FM="-ath10k-firmware-qca988x -ath10k-firmware-qca9888 -ath10k-firmware-qca9984"
 
